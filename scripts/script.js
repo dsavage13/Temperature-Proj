0198657ddf5ab3cf0bf871d39f6d9dab6d76dbda
dsavage13 <savage.damian@outlook.com>
2024-09-28 13:31:05 -0500

    Creating Variables

---
+++ scripts/script.js
@@ -0,0 +1,43 @@
+console.log("Script");
+
+// Creating String Variables
+//keyword nameVariable = value;
+let myName = "John";
+let lastName = "Doe";
+
+console.log(myName);
+console.log(lastName);
+console.log(myName + " " + lastName); //concatenation
+
+//Creating numerical variables
+let myAge = 25;
+console.log("Age: " + myAge);
+
+let myGrade = 9.5;
+console.log("Grade: " + myGrade);
+
+//Creating boolean variables (true/false)
+let isAdmin = true;
+let isStudent = false;
+let isProfessor = true;
+console.log(isProfessor);
+
+/*mutiple line comments*/
+//Single Line Comments
+
+myName="Damian";
+lastName="Savage";
+//My name is Damian Savage, and I am 25 years old.
+
+let number1 =10;
+let number2 =5;
+
+//addition
+let addition = number1 + number2;
+console.log(number1 + " + " number2 + " = " + addition);)
+
+//Prompt is a simple way to interact with the user
+
+let pet =prompt("Insert Your Pet Name");
+console.log("Your pet is: " + pet);
+
